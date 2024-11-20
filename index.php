bbacde53a011ad1e1fc001261fa347d971e9a20a
Abraham Sanchez <abraham_gto@hotmail.com>
2024-11-20 13:59:32 -0600

    LalaProyect

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+include "informacion.php";
+?>
+
+<html>
+
+<head>
+    <title>LALA</title>
+
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="left-section">
+            <form action="login.php" method="POST">
+                <input placeholder="Usuario" type="text" name="username" required name="usuario" />
+                <input placeholder="Contraseña" type="password" name="password" required name="password" />
+                <input type="submit" value="Ingresar" />
+            </form>
+        </div>
+        <div class="right-section">
+            <div class="overlay">
+                <h2>Sistema de Gestión para el Control de Transporte y Logística</h2>
+            </div>
+        </div>
+    </div>
+
+
+    <footer class="footer">
+        <p><?php echo date("l jS F Y"); ?> : <?php
+        date_default_timezone_set("America/Mexico_City");
+         echo date("h:i:sa "); ?></p>
+        <p class="footer__server">Nombre de la Estación de Trabajo: <span style="color:red;">
+                <?php echo $server_name; ?></span></p>
+        
+    </footer>
+
+
+</body>
+
+</html>
---
+++ php/menu.php
@@ -0,0 +1,55 @@
+<?php
+
+include "../informacion.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/menu.css">
+</head>
+
+<body>
+
+    <div class="header">
+        <p class="footer__estatus">Estatus De la Conexión:<span style="color:red;">
+                <?php echo $connection_status; ?></span></p>
+    </div>
+
+    <div class="container">
+        <div class="left">
+            <div class="userInfo">
+                <img src="../imgs/lalaIcon.png" class="lalaIcon">
+                <img src="../imgs/userImg.png" alt="" class="userImg">
+
+                <p class="userName"><?php echo htmlspecialchars($username); ?></p>
+            </div>
+
+        </div>
+        <div class="right">
+            <a href=""><img src="../imgs/Q3.png" alt="" srcset="" class="options"></a>
+            <a href=""><img src="../imgs/Q2.png" alt="" srcset="" class="options"></a>
+            <a href=""><img src="../imgs/Q1.png" alt="" srcset="" class="options"></a>
+            <a href=""><img src="../imgs/Q5.png" alt="" srcset="" class="options"></a>
+            <a href=""><img src="../imgs/Q4.png" alt="" srcset="" class="options"></a>
+        </div>
+
+    </div>
+
+    <footer class="footer">
+        <p><?php echo date("l jS F Y"); ?> : <?php
+        date_default_timezone_set("America/Mexico_City");
+         echo date("h:i:sa "); ?></p>
+        <p class="footer__server">Server name: <span style="color:red;"> <?php echo $server_name; ?></span></p>
+
+        <p class="footer__ip">Ip servidor Gestión:<span style="color:red;"> <?php echo $local_ip; ?></span></p>
+    </footer>
+</body>
+
+</html>
